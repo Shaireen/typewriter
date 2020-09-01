7b5d7ce110babdffa0e52026bb1656a80d737aae
Marcelina Jankowska <jankowskam401@gmail.com>
2020-09-01 13:09:20 +0200

    added sound effect

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 "use strict";
 const typewriteText = document.querySelector(".typewritten").textContent;
 const textContainer = document.querySelector(".placeholder");
+const audioOne = new Audio(
+  "Mechanical-Keyboard-single-button-presses-10-www.FesliyanStudios.com.mp3"
+);
+const audioTwo = new Audio(
+  "Mechanical-Keyboard-single-button-presses-1-www.FesliyanStudios.com.mp3"
+);
+const audioThree = new Audio(
+  "Mechanical-Keyboard-single-button-presses-2-www.FesliyanStudios.com.mp3"
+);
 let char = typewriteText.charAt(0);
 console.log(typewriteText.length);
 let i = 0;
-typewriteEffect();
+const playButton = document.querySelector(".startTypewrite");
+playButton.addEventListener("click", typewriteEffect);
+
+function getRandomNumber() {
+  return Math.random() * 50 + 200;
+}
 
 function typewriteEffect() {
   if (i < typewriteText.length) {
-    textContainer.textContent += typewriteText.charAt(i);
-    i++;
-    setTimeout(typewriteEffect, 100);
+    let characterAt = typewriteText.charAt(i);
+    textContainer.textContent += characterAt;
+    if (characterAt == " ") {
+      audioTwo.play();
+    } else if (getRandomNumber() < 225) {
+      audioOne.play();
+    } else {
+      audioThree.play();
+    }
   }
+  i++;
+  setTimeout(typewriteEffect, getRandomNumber());
 }
