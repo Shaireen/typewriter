d026ac43097241c4665a8a9e66b163c98d4cd7b5
Marcelina Jankowska <jankowskam401@gmail.com>
2020-09-01 12:09:10 +0200

    basic typewrite function working

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+const typewriteText = document.querySelector(".typewritten").textContent;
+const textContainer = document.querySelector(".placeholder");
+let char = typewriteText.charAt(0);
+console.log(typewriteText.length);
+let i = 0;
+typewriteEffect();
+
+function typewriteEffect() {
+  if (i < typewriteText.length) {
+    textContainer.textContent += typewriteText.charAt(i);
+    i++;
+    setTimeout(typewriteEffect, 100);
+  }
+}
